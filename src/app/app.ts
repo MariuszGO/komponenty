49b5006217c90cdefb6df53fd88aecff9cd6c2f7
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2025-11-19 11:54:10 +0100

    komponenty

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,4 +10,22 @@ imports: [syn, corka],
 templateUrl: './app.html',
  styleUrl: './app.css',
 })
-export class App {}
+export class App {
+
+zmiennaEksport: string[] = ['17:00', 'pływalnią'];
+
+ onOdpowiedz(otrzymane: string) {
+ alert(otrzymane);
+ }
+
+}
+
+
+/*
+# Generowanie podstawowego komponentu
+ng generate component nazwa-komponentu
+
+# Lub w skrócie:
+ng g c nazwa-komponentu
+
+*/
